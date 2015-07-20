28885501560434579a5f1816542d552db9ac6c1a
Tyler Gaw <me@tylergaw.com>
2015-07-20 09:32:23 -0400

    Heavy work on stardust abstraction

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 ObjC.import('Cocoa');
 
-import _ from 'underscore';
 import stardust from './stardust';
 
 var app = function() {
-  var ctrlsHeight = 80;
+  var ctrlsHeight = 100;
   var minWidth = 400;
   var minHeight = 340;
+
+  var logger = function(msg) {
+    $.NSLog('%@', msg);
+  };
 
   ObjC.registerSubclass({
     name: 'AppDelegate',
@@ -14,55 +17,71 @@ var app = function() {
       'click': {
         types: ['void', ['id']],
         implementation: function(sender) {
-          var panel = $.NSOpenPanel.openPanel;
-          panel.title = 'Choose an Image';
-          panel.allowedFileTypes = $(['jpg', 'png', 'gif']);
-
-          if (panel.runModal == $.NSOKButton) {
-            var imagePath = panel.URLs.objectAtIndex(0).path;
-            var img = $.NSImage.alloc.initByReferencingFile(imagePath);
-            var imgView = $.NSImageView.alloc.initWithFrame(
-              $.NSMakeRect(0, mainWindow.height(), img.size.width, img.size.height)
-            );
-
-            mainWindow.resize(
-              (img.size.width > minWidth) ? img.size.width : minWidth,
-              ((img.size.height > minHeight) ? img.size.height : minHeight) + ctrlsHeight
-            );
-
-            imgView.setImage(img);
-            mainWindow.append(imgView).center();
-          }
+          panel.open(fileSelected);
         }
       }
     }
   });
 
+  var fileSelected = function(panelUrls) {
+    let selectedFilePath = panelUrls[0].path;
+    let imgId = 'chosenImage';
+    let $img = $mainWindow.find('#' + imgId);
+
+    if ($img) {
+      $img.src(selectedFilePath);
+    } else {
+      $img = stardust.image({
+        id: imgId,
+        src: selectedFilePath,
+        rect: [0, $mainWindow.height()]
+      });
+
+      $mainWindow.append($img);
+    }
+
+    $mainWindow.find('#fileField').val(selectedFilePath);
+
+    $mainWindow.size(
+      ($img.width() > minWidth) ? $img.width() : minWidth,
+      (($img.height() > minHeight) ? $img.height() : minHeight) + ctrlsHeight
+    ).center();
+  };
+
   var appDelegate = $.AppDelegate.alloc.init;
 
-  var mainWindow = stardust.window({
+  var $mainWindow = stardust.window({
     title: 'Choose and Display Image',
     rect: [0, 0, 600, 85]
   }).delegate(appDelegate);
 
-  mainWindow.append(stardust.fieldLabel({
-    value: 'Image: (jpg, png, or gif)',
-    rect: [25, (mainWindow.height() - 40), 200, 24]
+  $mainWindow.append(stardust.fieldLabel({
+    id: 'fileFieldLabel',
+    value: 'Image: (jpg, png, or gif):',
+    rect: [25, ($mainWindow.height() - 40), 200, 24]
   }))
   .append(stardust.textField({
+    id: 'fileField',
     editable: false,
-    rect: [25, (mainWindow.height() - 60), 205, 24]
+    rect: [25, ($mainWindow.height() - 60), 350, 24]
   }))
   .append(stardust.button({
+    id: 'chooseButton',
     title: 'Choose an image...',
-    rect: [230, (mainWindow.height() - 62), 150, 25],
+    rect: [375, ($mainWindow.height() - 62), 150, 25],
     target: appDelegate,
     action: 'click'
   }));
 
+  var panel = stardust.openPanel({
+    title: 'Choose an Image',
+    allowedTypes: ['jpg', 'png', 'gif']
+  });
+
   return {
+    logger: logger,
     appDelegate: appDelegate,
-    mainWindow: mainWindow
+    mainWindow: $mainWindow
   }
 };
 
--- stardust.js
+++ stardust.js
@@ -1,12 +1,17 @@
 var stardust = {};
 
+let isFunction = function(func) {
+  var getType = {};
+  return func && getType.toString.call(func) === '[object Function]';
+}
+
 stardust.setOpts = function(defaults = {}, options = {}) {
   return Object.assign({}, defaults, options);
 };
 
-// This is a convenience method for $.NSMakeRect. While it's not that much easier
-// to type, it does allow for using the spread operator, which can't currently
-// by used directly with NSMakeRect.
+// This is a convenience method for $.NSMakeRect. While it's not that much
+// easier to type, it does allow for using the spread operator, which can't
+// currently be used directly with NSMakeRect.
 //
 // ex:
 //   var xywh = [20, 20, 200, 20];
@@ -15,24 +20,67 @@ stardust.makeRect = function(x, y, w, h) {
   return $.NSMakeRect(x, y, w, h);
 };
 
-// I want to be able to do this to add subviews
-stardust.append = function() {
-
-};
-
 stardust.window = function(settings) {
-  var opts = stardust.setOpts({
+  let opts = stardust.setOpts({
     title: 'My Application',
     rect: [0, 0, 200, 200]
   }, settings);
 
-  var styleMask = $.NSTitledWindowMask | $.NSClosableWindowMask |
+  let styleMask = $.NSTitledWindowMask | $.NSClosableWindowMask |
     $.NSResizableWindowMask | $.NSMiniaturizableWindowMask;
 
-  var obj = {
-    append(el) {
-      this.el.contentView.addSubview(el);
+  let obj = {
+    // View hierarchy
+    _h: [],
+
+    append(stardustEl) {
+      this._h.push(stardustEl);
+
+      this.el.contentView.addSubview(stardustEl.el);
       return this;
+    },
+
+    _findById(id) {
+      let c = this.children();
+      let $el = null;
+
+      for (let i = 0; i < c.length; i += 1) {
+        if (c[i].id() === id) {
+          $el = c[i];
+          break;
+        }
+      }
+
+      return $el;
+    },
+
+    find(selector) {
+      let $el = null;
+
+      if (!selector) {
+        return $el;
+      }
+
+      let firstChar = selector[0];
+      let isIdSelector = (firstChar === '#');
+      let isClassSelector = (firstChar === '.');
+
+      // Is this an ID or a class selector?
+      if (isIdSelector || isClassSelector) {
+        let search = selector.substr(1, selector.length);
+
+        if (isIdSelector) {
+          $el = this._findById(search);
+        }
+      }
+
+      return $el;
+    },
+
+    // Return the subviews of the contentView as a JavaScript Array.
+    // @return {Array}
+    children() {
+      return this._h;
     },
 
     title(t = null) {
@@ -89,7 +137,7 @@ stardust.window = function(settings) {
       }
     },
 
-    resize(w, h) {
+    size(w, h) {
       this.width(w);
       this.height(h);
       this.el.setFrameDisplay(
@@ -123,57 +171,390 @@ stardust.window = function(settings) {
   return obj;
 };
 
+stardust.openPanel = function(settings) {
+  let opts = stardust.setOpts({
+    title: 'Open'
+  }, settings);
+
+  let obj = {
+    title (t = null) {
+      if (t) {
+        this.el.title = t;
+        return this;
+      } else {
+        return this.el.title;
+      }
+    },
+
+    /**
+     * @param t {Array}
+     */
+    allowedTypes (t = null) {
+      if (t) {
+        // We use `$()` to convert the JS array to an NSArray
+        this.el.allowedFileTypes = $(t);
+        return this;
+      } else {
+        return this.el.allowedFileTypes;
+      }
+    },
+
+    open (ok = null) {
+      if (this.el.runModal == $.NSOKButton) {
+        if (ok && isFunction(ok)) {
+          // Pass back a JS Array instead of the NSArray that the openPanel
+          // will return.
+          ok($(this.el.URLs).js);
+        }
+      }
+    },
+
+    _id: null,
+    id(i = null) {
+      if (i) {
+        this._id = i;
+        return this;
+      } else {
+        return this._id;
+      }
+    }
+  };
+
+  obj.el = $.NSOpenPanel.openPanel;
+  obj.classString = $.NSStringFromClass(obj.el.class).js;
+
+  obj.title(opts.title);
+
+  if (opts.allowedTypes) {
+    obj.allowedTypes(opts.allowedTypes);
+  }
+
+  if (opts.id) {
+    obj.id(opts.id);
+  }
+
+  return obj;
+};
+
 stardust.button = function(settings) {
-  var opts = stardust.setOpts({
+  let opts = stardust.setOpts({
     title: 'Button',
     rect: [25, 25, 150, 24],
+    bezelStyle: $.NSRoundedBezelStyle,
+    buttonType: $.NSMomentaryLightButton,
     target: null,
     action: null
   }, settings);
 
-  var button = $.NSButton.alloc.initWithFrame(stardust.makeRect(...opts.rect));
+  let obj = {
+    title(t = null) {
+      if (t) {
+        this.el.title = t;
+        return this;
+      } else {
+        return this.el.title;
+      }
+    },
 
-  button.title = opts.title;
-  button.bezelStyle = $.NSRoundedBezelStyle;
-  button.buttonType = $.NSMomentaryLightButton;
+    bezelStyle(b = null) {
+      if (b) {
+        this.el.bezelStyle = b;
+        return this;
+      } else {
+        return this.el.bezelStyle;
+      }
+    },
+
+    buttonType(b = null) {
+      if (b) {
+        this.el.buttonType = b;
+        return this;
+      } else {
+        return this.el.buttonType;
+      }
+    },
+
+    target(t = null) {
+      if (t) {
+        this.el.target = t;
+        return this;
+      } else {
+        return this.el.target;
+      }
+    },
+
+    action(a = null) {
+      if (a) {
+        this.el.action = a;
+        return this;
+      } else {
+        return this.el.action;
+      }
+    },
+
+    _id: null,
+    id(i = null) {
+      if (i) {
+        this._id = i;
+        return this;
+      } else {
+        return this._id;
+      }
+    }
+  };
+
+  obj.el = $.NSButton.alloc.initWithFrame(stardust.makeRect(...opts.rect));
+  obj.classString = $.NSStringFromClass(obj.el.class).js;
+
+  obj.title(opts.title);
+  obj.bezelStyle(opts.bezelStyle);
 
   if (opts.target) {
-    button.target = opts.target;
+    obj.target(opts.target);
   }
 
   if (opts.action) {
-    button.action = opts.action;
+    obj.action(opts.action);
   }
 
-  return button;
+  if (opts.id) {
+    obj.id(opts.id);
+  }
+
+  return obj;
 };
 
 stardust.textField = function(settings) {
-  var opts = stardust.setOpts({
+  let opts = stardust.setOpts({
     editable: true,
     rect: [25, 25, 200, 24]
   }, settings);
 
-  var field = $.NSTextField.alloc.initWithFrame(stardust.makeRect(...opts.rect));
+  let obj = {
+    editable (b = null) {
+      if (b !== null) {
+        this.el.editable = b;
+        return this;
+      } else {
+        return this.el.editable;
+      }
+    },
 
-  field.editable = opts.editable;
-  return field;
+    _id: null,
+    id(i = null) {
+      if (i) {
+        this._id = i;
+        return this;
+      } else {
+        return this._id;
+      }
+    },
+
+    val (v = null) {
+      if (v) {
+        this.el.stringValue = v;
+        return this;
+      } else {
+        return this.el.stringValue;
+      }
+    },
+  };
+
+  obj.el = $.NSTextField.alloc.initWithFrame(stardust.makeRect(...opts.rect));
+  obj.classString = $.NSStringFromClass(obj.el.class).js;
+
+  if (opts.id) {
+    obj.id(opts.id);
+  }
+
+  obj.editable(opts.editable);
+
+  return obj;
 };
 
 stardust.fieldLabel = function(settings) {
-  var opts = stardust.setOpts({
+  let opts = stardust.setOpts({
     value: 'Field label',
-    rect: [25, 25, 200, 24]
+    rect: [25, 25, 200, 24],
+    drawsBackground: false,
+    bezeled: false,
+    selectable: true
   }, settings);
 
-  var label = $.NSTextField.alloc.initWithFrame(stardust.makeRect(...opts.rect));
+  let obj = {
+    val (v = null) {
+      if (v) {
+        this.el.stringValue = v;
+        return this;
+      } else {
+        return this.el.stringValue;
+      }
+    },
 
-  label.stringValue = opts.value;
-  label.drawsBackground = false;
-  label.editable = false;
-  label.bezeled = false;
-  label.selectable = true;
-  return label;
+    drawsBackground (b = null) {
+      if (b !== null) {
+        this.el.drawsBackground = b;
+        return this;
+      } else {
+        return this.el.drawsBackground;
+      }
+    },
+
+    bezeled (b = null) {
+      if (b !== null) {
+        this.el.bezeled = b;
+        return this;
+      } else {
+        return this.el.bezeled;
+      }
+    },
+
+    selectable (s = null) {
+      if (s !== null) {
+        this.el.selectable = s;
+        return this;
+      } else {
+        return this.el.selectable;
+      }
+    },
+
+    _id: null,
+    id(i = null) {
+      if (i) {
+        this._id = i;
+        return this;
+      } else {
+        return this._id;
+      }
+    }
+  };
+
+  obj.el = $.NSTextField.alloc.initWithFrame(stardust.makeRect(...opts.rect));
+  obj.classString = $.NSStringFromClass(obj.el.class).js;
+
+  obj.val(opts.value);
+  obj.drawsBackground(opts.drawsBackground);
+  obj.bezeled(opts.bezeled);
+  obj.selectable(opts.selectable);
+
+  if (opts.id) {
+    obj.id(opts.id);
+  }
+
+  // Since this is a fieldLabel, don't allow `editable` to be set.
+  obj.el.editable = false;
+
+  return obj;
+};
+
+stardust.image = function(settings) {
+  let opts = stardust.setOpts({
+    src: null
+  }, settings);
+
+  let obj = {
+    rect: [0, 0, 0, 0],
+
+    src(s = null) {
+      if (s) {
+        let img = s;
+
+        // If we received a string, we need to convert it to an NSImage
+        if (s.isKindOfClass($.NSString)) {
+          img = $.NSImage.alloc.initByReferencingFile(s);
+        }
+
+        this.el.setImage(img);
+
+        this.width(img.size.width)
+          .height(img.size.height)
+          .size();
+
+        return this;
+      } else {
+        return this.el.image;
+      }
+    },
+
+    pos(p = null) {
+      if (p) {
+        this.rect[0] = p.x;
+        this.rect[1] = p.y;
+      } else {
+        return {
+          x: this.rect[0],
+          y: this.rect[1]
+        };
+      }
+    },
+
+    width(w = null) {
+      if (w) {
+        this.rect[2] = w;
+        return this;
+      } else {
+        return this.rect[2];
+      }
+    },
+
+    height(h = null) {
+      if (h) {
+        this.rect[3] = h;
+        return this;
+      } else {
+        return this.rect[3];
+      }
+    },
+
+    size(w, h) {
+      this.el.setFrameSize({
+        width: this.width(),
+        height: this.height()
+      })
+
+      return this;
+    },
+
+    _id: null,
+    id(i = null) {
+      if (i) {
+        this._id = i;
+        return this;
+      } else {
+        return this._id;
+      }
+    }
+  };
+
+  obj.pos({
+    x: opts.rect[0],
+    y: opts.rect[1]
+  });
+
+  obj.el = $.NSImageView.alloc.initWithFrame(stardust.makeRect(...obj.rect));
+  obj.classString = $.NSStringFromClass(obj.el.class).js;
+
+  if (opts.src) {
+    obj.src(opts.src);
+  }
+
+  // If a width and/or height has been provided, those should always take
+  // precedence over the width/height of any src image.
+  if (opts.rect[2]) {
+    obj.width(opts.rect[2]);
+  }
+
+  if (opts.rect[3]) {
+    obj.height(opts.rect[3]);
+  }
+
+  if (opts.id) {
+    obj.id(opts.id);
+  }
+
+  obj.size();
+
+  return obj;
 };
 
 export default stardust;
